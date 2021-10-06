cf18b7006687525ca64639f7f8ee2044887e6959
archana.r1 <archana.r1@tataelxsi.co.in>
2021-10-06 13:12:23 +0530

    Intro to services and pipes

--- tutorial/src/app/app.component.ts
+++ tutorial/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tutorial';
+  title = 'Hello world How are you ?';
+  jsonValue = {
+    test: 123,
+  };
+  date = new Date();
+
+  handleClick() {
+    console.log(this.title);
+  }
 }
