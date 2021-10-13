b03bbd780f4a4ea2cf501b715454a416733cb78d
archana.r1 <archana.r1@tataelxsi.co.in>
2021-10-13 14:06:17 +0530

    custom pipe with no args

--- pipes-start/src/app/app.module.ts
+++ pipes-start/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ShortenPipe } from './shortenPipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortenPipe
   ],
   imports: [
     BrowserModule,
