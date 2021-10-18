d1b41c3e6f4ebc5db6f1054939a5d20ce9ea5cc1
archana.r1 <archana.r1@tataelxsi.co.in>
2021-10-18 14:34:28 +0530

    http practice for fetching posts

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+import {map} from 'rxjs/operators'
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +10,42 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Array<Post> = [];
+  isFetching: boolean = false
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  @ViewChild('postForm') formRef: NgForm;
+  ngOnInit() {
+    this.onFetchPosts()
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http.post('https://udemy-http-1c237-default-rtdb.firebaseio.com/posts.json', postData).subscribe(post => {
+      this.formRef.form.reset()
+    })
   }
 
   onFetchPosts() {
     // Send Http request
+    this.isFetching = true;
+    this.http.get('https://udemy-http-1c237-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map((data: {[key: string]: Post}) => {
+      const postsArray: Post[] = []
+      for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+          postsArray.push({
+            ...data[key], id: key
+          })
+        }
+      }
+      return postsArray
+    }))
+    .subscribe(posts => {
+      this.loadedPosts = posts
+      this.isFetching = false
+    })
   }
 
   onClearPosts() {
