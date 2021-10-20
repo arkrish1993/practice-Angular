d9f56587be8440d0a2658ae075d552d82dd4baf9
archana.r1 <archana.r1@tataelxsi.co.in>
2021-10-20 11:36:44 +0530

    Navigation

--- angular-tour-of-heroes/src/app/components/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/components/hero-detail/hero-detail.component.ts
@@ -1,4 +1,7 @@
+import { Location } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from 'src/app/services/hero.service';
 import { Hero } from '../../interfaces/hero';
 
 @Component({
@@ -10,9 +13,24 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero?: Hero;
   
-  constructor() { }
+  constructor(
+    private location: Location,
+    private route: ActivatedRoute,
+    private heroService: HeroService
+  ) { }
 
   ngOnInit(): void {
+    this.getHero()
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe((hero: Hero) => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
