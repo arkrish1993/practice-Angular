6c2504c91d205f5ae573a34d278def91b99da83f
archana.r1 <archana.r1@tataelxsi.co.in>
2021-10-27 10:36:50 +0530

    Auth Service cleanup

--- udemy-recipe-builder/src/app/auth/auth.component.ts
+++ udemy-recipe-builder/src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -28,28 +29,20 @@ export class AuthComponent {
     this.error = '';
     const email = this.authForm.value.email;
     const password = this.authForm.value.password;
+    let authObs: Observable<AuthResponseData>;
     if (this.isLoginMode) {
-      this.authService.login(email, password).subscribe(
-        (resData) => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        (error) => {
-          this.error = error;
-          this.isLoading = false;
-        }
-      );
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(
-        (resData) => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        (error) => {
-          this.error = error;
-          this.isLoading = false;
-        }
-      );
+      authObs = this.authService.signup(email, password);
     }
+    authObs.subscribe(
+      (resData) => {
+        this.isLoading = false;
+      },
+      (error) => {
+        this.error = error;
+        this.isLoading = false;
+      }
+    );
   }
 }
--- udemy-recipe-builder/src/app/auth/auth.service.ts
+++ udemy-recipe-builder/src/app/auth/auth.service.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -27,18 +28,7 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(
-        catchError((error) => {
-          let errorMessage = 'An unknown error occurred!';
-          if (error.error && error.error.error) {
-            switch (error.error.error.message) {
-              case 'EMAIL_EXISTS':
-                errorMessage = 'This email exists already!';
-            }
-          }
-          return throwError(errorMessage);
-        })
-      );
+      .pipe(catchError(this.handleError));
   }
 
   login(email: string, password: string): Observable<AuthResponseData> {
@@ -51,11 +41,24 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(
-        catchError((error) => {
-          let errorMessage = 'An unknown error occurred!';
-          return throwError(errorMessage);
-        })
-      );
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (errorRes.error && errorRes.error.error) {
+      switch (errorRes.error.error.message) {
+        case 'EMAIL_EXISTS':
+          errorMessage = 'This email exists already!';
+          break;
+        case 'EMAIL_NOT_FOUND':
+          errorMessage = 'This email does not exist!';
+          break;
+        case 'INVALID_PASSWORD':
+          errorMessage = 'Invalid password!';
+          break;
+      }
+    }
+    return throwError(errorMessage);
   }
 }
